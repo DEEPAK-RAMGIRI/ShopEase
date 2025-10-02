127e70cf882a5d9bcaadf69ac379966e3efefb45
RAMGIRI  DEEPAK KUMAR <142658497+DEEPAK-RAMGIRI@users.noreply.github.com>
2025-10-02 19:16:53 +0530

    Fix case of product model import

--- Foodies/Backend/routes/api.routes.js
+++ Foodies/Backend/routes/api.routes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-const Product = require('../models/Product.model');
+const Product = require('../models/product.model');
 const Cart = require('../models/cart.model');
 
 router.get('/products', async(req,res) => {
