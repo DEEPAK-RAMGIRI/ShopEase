4475956853bc4b55ab8b8ae8858f63cf9c7cbe4d
DEEPAK-RAMGIRI <ramgirideepak89@gmail.com>
2025-10-02 19:10:46 +0530

    while depolying backend faced an error

--- Foodies/Backend/server.js
+++ Foodies/Backend/server.js
@@ -7,7 +7,7 @@ require('dotenv').config();
 const PORT = process.env.PORT || 3001;
 const MONGO_URI = process.env.MONGO_URI;
 
-const Product = require('./models/Product.model');
+const Product = require('./models/product.model');
 const apiRoutes = require('./routes/api.routes');
 
 const app = express();
